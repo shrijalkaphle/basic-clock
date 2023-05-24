8d3bf37ef4cef91d3599f27c9e928050dfc5971c
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2023-05-24 19:25:11 +0545

    weather added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import axios from 'axios';
 import moment from "moment";
 import { useEffect, useState } from "react";
 import NepaliDate from 'nepali-date-converter'
+
+interface IWeatherData {
+  cloudcover: number
+  conditions: string
+  datetime: string
+  feelslike: number
+  humidity: number
+  icon: string
+  temp: number
+  windspeed: number
+}
 
 function App() {
   const element = document.documentElement
@@ -9,8 +21,19 @@ function App() {
   const [time, setTime] = useState<string>(moment().format())
   const [nepaliDate, setNepaliDate] = useState(new NepaliDate())
 
+  const [weatherData, setWeatherData] = useState<IWeatherData>({
+    cloudcover: 25,
+    conditions: "Partially cloudy",
+    datetime: "18:15:00",
+    feelslike: 25,
+    humidity: 61.2,
+    icon: "partly-cloudy-day",
+    temp: 25,
+    windspeed: 11.2
+  })
+
   const fullScreenPreivew = () => {
-    if(isFullScreen) {
+    if (isFullScreen) {
       setIsFullScreen(false)
       //close
       if (document.exitFullscreen)
@@ -22,8 +45,16 @@ function App() {
     }
   }
 
+  const weatherApiRequest = () => {
+    axios.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Kathmandu,NP?key=JT8L6SDRS5CUUM4U338CCBY7E&unitGroup=metric')
+      .then((response) => {
+        console.log(response)
+        setWeatherData(response.data.currentConditions)
+      })
+  }
+
   const HourToggle = () => {
-    if(is24Hour) {
+    if (is24Hour) {
       setIs24Hour(false)
     } else {
       setIs24Hour(true)
@@ -31,11 +62,19 @@ function App() {
   }
 
   useEffect(() => {
+    weatherApiRequest()
     const interval = setInterval(() => {
       setTime(moment().format())
       setNepaliDate(new NepaliDate())
     }, 500);
-    return () => clearInterval(interval)
+
+    const weatherInterval = setInterval(() => {
+      weatherApiRequest()
+    },120000)
+    return () => {
+      clearInterval(interval)
+      clearInterval(weatherInterval)
+    }
   }, [])
 
   return (
@@ -46,16 +85,32 @@ function App() {
           <div className="flex gap-x-3 absolute top-1 right-1">
             <button className="border px-2 py-1 rounded-lg" onClick={fullScreenPreivew}>Toogle Fullscreen</button>
             <button className="border px-2 py-1 rounded-lg" onClick={HourToggle}>24 hour Toogle</button>
+            <button className="border px-2 py-1 rounded-lg" onClick={weatherApiRequest}>weather</button>
           </div>
           <div>
             <div className="flex items-end justify-center">
               <div className="text-9xl">
-                {is24Hour ? moment(time).format('HH') :moment(time).format('hh') }:{moment(time).format('mm')}</div>
+                {is24Hour ? moment(time).format('HH') : moment(time).format('hh')}:{moment(time).format('mm')}</div>
               <div className="text-6xl mb-3 ml-2">: {moment(time).format('ss')}</div>
-              { !is24Hour && <div className="text-7xl mb-3 ml-8">{moment(time).format('A')}</div>}
+              {!is24Hour && <div className="text-7xl mb-3 ml-8">{moment(time).format('A')}</div>}
             </div>
-            <div className="mt-6 text-5xl text-center">
+            <div className="mt-6 text-5xl text-center mb-6">
               {moment().format("dddd | MMMM DD, YYYY")} | {nepaliDate.format("DD MMMM, YYYY").toString()}
+            </div>
+            <hr />
+            <div className='mt-6 justify-center flex items-center gap-x-4'>
+              <div>
+                <img src={`./svgs/${weatherData.icon}.svg`} className='h-42 w-42' />
+              </div>
+              <div className='text-center'>
+                <div className='text-xl'>{weatherData.conditions}</div>
+                <div className='text-2xl mt-2'>{weatherData.temp}&deg;C <span className='text-lg'>(Feels like {weatherData.feelslike}&deg;C)</span></div>
+                <div className='text-lg mt-2'>
+                  <span className='block'>Humidity: {weatherData.humidity}%</span>
+                  <span className='block'>Wind Speed: {weatherData.windspeed} km/h</span>
+                  <span className='block text-sm'>{weatherData.datetime}</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
