bb0477f7067587ec19ee3668b7aafd5fe3f24fdd
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2023-05-24 16:53:19 +0545

    added nepali date

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import moment from "moment";
 import { useEffect, useState } from "react";
+import NepaliDate from 'nepali-date-converter'
 
 function App() {
   const element = document.documentElement
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false)
   const [is24Hour, setIs24Hour] = useState<boolean>(false)
-  const [hour, setHour] = useState(moment().format("hh"))
-  const [minute, setMinute] = useState(moment().format("mm"))
-  const [second, setSecond] = useState(moment().format("ss"))
-  const [day, setDay] = useState(moment().format("A"))
+  const [time, setTime] = useState<string>(moment().format())
+  const [nepaliDate, setNepaliDate] = useState(new NepaliDate())
 
   const fullScreenPreivew = () => {
     if(isFullScreen) {
@@ -33,10 +32,7 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      is24Hour ? setHour(moment().format("HH")): setHour(moment().format("hh"));
-      setMinute(moment().format("mm"));
-      setSecond(moment().format("ss"));
-      setDay(moment().format("A"))
+      setTime(moment().format())
     }, 500);
     return () => clearInterval(interval)
   }, [])
@@ -52,12 +48,13 @@ function App() {
           </div>
           <div>
             <div className="flex items-end justify-center">
-              <div className="text-9xl">{hour}:{minute}</div>
-              <div className="text-6xl mb-3 ml-2">: {second}</div>
-              { !is24Hour && <div className="text-7xl mb-3 ml-8">{day}</div>}
+              <div className="text-9xl">
+                {is24Hour ? moment(time).format('HH') :moment(time).format('hh') }:{moment(time).format('mm')}</div>
+              <div className="text-6xl mb-3 ml-2">: {moment(time).format('ss')}</div>
+              { !is24Hour && <div className="text-7xl mb-3 ml-8">{moment(time).format('A')}</div>}
             </div>
             <div className="mt-6 text-5xl text-center">
-              {moment().format("dddd | MMMM DD, YYYY")}
+              {moment().format("dddd | MMMM DD, YYYY")} | {nepaliDate.format("DD MMMM, YYYY").toString()}
             </div>
           </div>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './tailwind.css'
+import './font.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
